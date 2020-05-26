f8f2c7ff04743568ab971b8ea708e7312683e6d7
marta-canizares <marta.s.canizares@gmail.com>
2020-05-26 12:06:37 +0200

    done

--- index.js
+++ index.js
@@ -5,15 +5,63 @@ export default class SortedList {
         this.length = 0;
     }
 
-    add(item) { }
+    add(item) {
+        this.items.push(item);
+        this.items.sort(function (a, b) { return a - b });
+        this.length = this.items.length;
+    }
 
-    get(pos) { }
 
-    max() { }
+    get(pos) {
+        if (pos > this.items.length) {
+            throw new Error('OutOfBounds');
+        } else {
+            return this.items[pos];
+        }
+    }
 
-    min() { }
+    max() {
 
-    sum() { }
+        if (this.items.length === 0) {
+            throw new Error('EmptySortedList');
+        } else {
+            return this.items[this.items.length - 1] // devuelve el valor de la ultima posicion del array
+        }
+    }
 
-    avg() { }
+    min() {
+        if (this.items.length === 0) {
+            throw new Error('EmptySortedList');
+        } else {
+            return this.items[0]; // devuelve el valor de la primera posicion del array
+        }
+
+    }
+
+    sum() {
+        let pos = 0;
+
+        if (this.items.length === 0) {
+            return 0;
+        } else {
+            this.items.forEach(element => {
+                return pos += element;
+            });
+            return pos;
+        }
+
+    }
+
+    avg() {
+        let pos = 0;
+
+        if (this.items.length === 0) {
+            throw new Error('EmptySortedList');
+        } else {
+            this.items.forEach(element => {
+                return pos += element;
+            });
+            return pos / this.items.length;
+        }
+    }
 }
